167403cc4cbbeef396bc7bede3113b6cea7798b9
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2025-10-04 09:56:14 -0400

    Initial commit

---
+++ src/main/java/org/chrisbarbati/spaceapps2025backend/SpaceApps2025BackendApplication.java
@@ -0,0 +1,13 @@
+package org.chrisbarbati.spaceapps2025backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceApps2025BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceApps2025BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/chrisbarbati/spaceapps2025backend/SpaceApps2025BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chrisbarbati.spaceapps2025backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceApps2025BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
